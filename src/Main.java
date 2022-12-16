48c4f6611875e491042f408c2f74c2b0e11eab07
glebov <constantine.glebov@gmail.com>
2022-12-16 14:12:17 +0700

    Description and condition of the assignment

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
 
+        Calculator calc = Calculator.instance.get();
+
+        int a = calc.plus.apply(1, 2);
+        int b = calc.minus.apply(1, 1);
+        int c = calc.devide.apply(a, b); //строка не работает т.к. происходит деление на ноль (ArithmeticException: / by zero)
+
+        calc.println.accept(c);
+
     }
+}
+
+@FunctionalInterface
+interface Supplier<C> {
+    Calculator get();
 }
